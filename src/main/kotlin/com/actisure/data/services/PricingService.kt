333a41de7115ea4dc26522a0d22813f9a9395500
Cosmas <pkiruicos@gmail.com>
2023-02-18 14:05:26 +0300

    Kotlin & Spring Boot oracle API

--- src/main/kotlin/com/actisure/data/services/PricingService.kt
+++ src/main/kotlin/com/actisure/data/services/PricingService.kt
@@ -3,6 +3,7 @@ import com.actisure.data.controllers.messages.Pricing
 import com.actisure.data.repositories.PricingRepository
 import org.springframework.stereotype.Service
 @Service
+
 class PricingService(val db: PricingRepository) {
    fun findMessages(): List<Pricing> = db.findMessages()
 
