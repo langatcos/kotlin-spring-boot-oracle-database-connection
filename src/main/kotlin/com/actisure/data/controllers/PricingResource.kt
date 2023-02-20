d7467e5178262aaddf4570f8263b9d41f85a787c
Cosmas <pkiruicos@gmail.com>
2023-02-20 10:13:08 +0300

    Kotlin & Spring Boot oracle API

--- src/main/kotlin/com/actisure/data/controllers/PricingResource.kt
+++ src/main/kotlin/com/actisure/data/controllers/PricingResource.kt
@@ -3,6 +3,9 @@ package com.actisure.data.controllers
 import com.actisure.data.repositories.PricingRepository
 import com.actisure.data.benefitcomponents.BenefitComponent
 import com.actisure.data.benefitcomponents.PriceDetails
+import com.actisure.data.controllers.messages.Pricing
+import com.actisure.data.controllers.messages.RenewalList
+import com.actisure.data.services.PricingService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -27,6 +30,34 @@ class PricingResource(val messageRepository: PricingRepository) {
 
         return ResponseEntity.ok(components)
     }
+    @GetMapping("/getallpricing")
+    fun index(): List<Map<String, Any>> {
+        val pricingList = messageRepository.findMessages()
+        val groupedPricing = pricingList.groupBy { it.COMPONENTID }
+        val result = mutableListOf<Map<String, Any>>()
+        groupedPricing.forEach { (componentid, pricings) ->
+            val priceDetails = pricings.map {
+                mapOf(
+                    "agefrom" to it.AGEFROM,
+                    "ageto" to it.AGETO,
+                    "principleOrChildRate" to it.PRINCIPLE_CHILD,
+                    "spouserate" to it.SPOUSERATE
+                )
+            }
+
+            val pricingMap = mapOf(
+                "componenttypeid" to pricings.first().COMPONENTID,
+                "componentid" to componentid,
+                "description" to pricings.first().DESCRIPTION,
+                "priceDetails" to priceDetails
+            )
+
+            result.add(pricingMap)
+        }
+
+        return result
+    }
+
 }
 /*@RestController
 class MessageResource(val service: MessageService, val messageRepository: MessageRepository) {
